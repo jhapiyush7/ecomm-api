c1a6496bc2e286eab66327fdd0228544f72db0f2
Piyush <piyush.raj0@walmart.com>
2022-03-15 20:55:54 +0530

    commit

--- backend/configuration/database.js
+++ backend/configuration/database.js
@@ -3,11 +3,7 @@ const mongoose=require('mongoose');
 
 const connectDatabase=()=>{
     mongoose
-      .connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopoplogy: true,
-        useCreateIndex: true,
-      })
+      .connect(process.env.DB_URI)
       .then((data) => {
         console.log(`Mongodb connected with server:${data.connection.host}`);
       })
