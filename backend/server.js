c77f1be68e6b4e62e210fed9f39eeeb48fee55e6
Piyush <piyush.raj0@walmart.com>
2022-03-15 18:10:54 +0530

    db config

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const app=require("./app");
 
 const dotenv=require("dotenv");
+const connectDatabase=require('./configuration/database')
 
+//configuration
 dotenv.config({path:"backend/configuration/config.env"});
 
+//connecting to db
+
+connectDatabase();
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
